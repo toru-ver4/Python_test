0419b4601d14046edeb7c32409be08f680c46faa
Toru Yoshihara <Toru Yoshihara>
2016-04-04 21:05:56 +0900

    aaaa

--- scraping/sample.py
+++ scraping/sample.py
@@ -7,32 +7,42 @@ test for request module.
 
 import os
 import sys
-import time
-import json
-import requests
-from selenium import webdriver
+from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-NICO_LOGIN_URL = "https://account.nicovideo.jp/api/v1/login?site=niconico&next_url="
-NICO_TOP_URL = "http://www.nicovideo.jp/"
 
-def print_cp932(in_str):
-    out_str = in_str
-    print(out_str)
+def exec_beautifulsoup_sample():
+
+    try:
+        html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+    except HTTPError as e:
+        print(e)
+    except URLError as e:
+        print("The server could not be found!")
+    else:
+        bsObj = BeautifulSoup(html.read(), 'lxml')
+        print(bsObj.h1)
+
+
+def getTitle(url):
+    """try, except をきちんと書いてプログラムが止まらないように"""
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), 'lxml')
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+
+    return title
+
 
 if __name__ == '__main__':
-
-    with open('login_info.json', 'r') as fin:
-        json_data = json.load(fin)
-
-    s = requests.Session()
-    driver = webdriver.PhantomJS()
-    driver.get("https://account.nicovideo.jp/login")
-    html = driver.page_source.encode('utf-8')
-    soup = BeautifulSoup(html, "lxml")
-    auth_token = soup.find(attrs={'name': 'auth_id'}).get('value')
-    print(auth_token)
-#    json_data['auth_id'] = str(auth_token) # it is not needed... OMG.
-    r = s.post(NICO_LOGIN_URL, data=json_data)
-    print(r.text.find('taku-ver4'))
-
+    # exec_beautifulsoup_sample()
+    getTitle("http://www.pythonscraping.com/pages/page1.html")
+    getTitle("http://wwww.pythonscraping.com/pages/page1.html")
