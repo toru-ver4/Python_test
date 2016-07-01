d6d2c4e8e52a34e150a5134f81a51f8f040ec824
Toru Yoshihara <Toru Yoshihara>
2016-07-01 21:36:55 +0900

    ScrollViewに画像を表示するよう修正。

--- scraping/twitter/img_view/main.py
+++ scraping/twitter/img_view/main.py
@@ -1,12 +1,28 @@
+import os
+import fnmatch
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 from kivy.lang import Builder
+from kivy.uix.image import Image
 
 Builder.load_file('./tview.kv')
 
+
 class MyWidget(Widget):
-    pass
+    def add_picture(self):
+        img_dir = './img_cache'
+        img_full_path_list = []
+        for file_name in os.listdir(img_dir):
+            if fnmatch.fnmatch(file_name, '*.jpg'):
+                img_full_path_list.append(os.path.join(img_dir, file_name))
+
+        for file_name in img_full_path_list:
+            im = Image(source=file_name, size_hint=(1, None))
+            self.ids.illust_area.add_widget(im)
+
+    def clear_picture(self):
+        self.ids.illust_area.clear_widgets()
 
 
 class TwitterViewApp(App):
@@ -18,9 +34,6 @@ class TwitterViewApp(App):
     def close_callback(self):
         print("this application to be close...")
         self.stop()
-
-    def clear_picture(self):
-        print('cleaning the pallet.')
 
 
 if __name__ == '__main__':
