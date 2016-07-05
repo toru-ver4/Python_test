79dd26a0da10609294892825a10060bd9f19c4e5
Toru Yoshihara <Toru Yoshihara>
2016-07-05 20:40:11 +0900

    Textボックスに入れたtwitter_id のユーザの画像を表示できるように改良した！

--- scraping/twitter/img_view/main.py
+++ scraping/twitter/img_view/main.py
@@ -4,8 +4,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.image import Image
+import re
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from datetime import datetime
 
 Builder.load_file('./tview.kv')
+
+const_img_pattern = re.compile(".*\/media\/.*jpg")
 
 
 class MyWidget(Widget):
@@ -18,11 +25,45 @@ class MyWidget(Widget):
 
         for file_name in img_full_path_list:
             im = Image(source=file_name, size_hint_y=None, height=300)
-            # im = Image(source=file_name, size=(400, 400))
             self.ids.illust_area.add_widget(im)
 
     def clear_picture(self):
         self.ids.illust_area.clear_widgets()
+
+    def get_tiwtter_illust(self):
+        twitter_id = self.ids.id_text_box.text
+        url = "https://twitter.com/" + twitter_id + "/media"
+        print(url)
+        temp_dir = './img_cache'
+        r = requests.get(url)
+        driver = webdriver.PhantomJS()
+        driver.get(url)
+
+        # 以下は必要に応じてコールすること
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # time.sleep(3)
+
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+
+        for idx, img_tag in \
+                enumerate(soup.findAll("img", {'src': const_img_pattern})):
+            img_url = img_tag["src"]
+            utc_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+            temp_file_name = 'temp_img_' + utc_time + str(idx) + "_.jpg"
+
+            # いったん、Tempとして保存
+            r = requests.get(img_url, stream=True)
+            if r.status_code == 200:
+                file_full_name = os.path.join(temp_dir, temp_file_name)
+                with open(file_full_name, 'wb') as f:
+                    for chunk in r.iter_content(1024):
+                        f.write(chunk)
+                # 少々ダサいが、ファイルを再オープンして画像を表示
+                # ----------------------------------------
+                im = Image(source=file_full_name, size_hint_y=None, height=300)
+                self.ids.illust_area.add_widget(im)
+
+        driver.close()
 
 
 class TwitterViewApp(App):
